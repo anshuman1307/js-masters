49794a7c98dee3595bf463d464043514a8c4a171
unknown <anshuman1307@yahoo.co.in>
2021-05-01 05:50:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { data } from "./data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="header">JS Masters</header>
+      <nav></nav>
+      <div className="container">
+        {data.map((val) => {
+          return (
+            <div className="question-list">
+              <div className="card w-75">
+                <div className="card-body">
+                  <h5 className="card-title">{val.title}</h5>
+                  <p className="card-text">{val.question}</p>
+                  <div className="inptotpt">
+                  <p className="input">&#8226;	Input : {val.input}</p>
+                  <p className="output">&#8226;	Output : {val.output}</p>
+                  </div>              
+                  <a href="#" class="btn btn-success">
+                    View Code
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,36 @@
+ export const data = [{
+    "id":1,
+    "title":"Binary array sorting",
+    "question":"Given a binary array, sort it using one traversal and no extra space.",
+    "input":"1 0 0 1 0 1 0 1 1 1 1 1 1 0 0 1 1 0 1 0 0",
+    "output":"0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1"
+},
+{
+    "id":2,
+    "title":"Cash withdrawal from ATM machine",
+    "question":" Find minimum number of currency notes and values that sum to given amount",
+    "input":"800",
+    "output":"Currency Count-> 500 : 1, 200 : 1, 100 : 1"
+},
+{
+    "id":3,
+    "title":"Anagrams",
+    "question":" Check whether two strings are anagram of each other",
+    "input":" s = 'anagram', t = nagaram'",
+    "output":"true"
+},
+{
+    "id":4,
+    "title":"Function Currying",
+    "question":" Implement function currying to execute below function",
+    "input":" total(1)(2)(3)(4)(5)",
+    "output":"15"
+},
+{
+    "id":5,
+    "title":"Minimum Number of Platforms Required for a Railway/Bus Station",
+    "question":"Given the arrival and departure times of all trains that reach a railway station, the task is to find the minimum number of platforms required for the railway station so that no train waits. ",
+    "input":" arr[] = {9:00, 9:40, 9:50, 11:00, 15:00, 18:00} dep[] = {9:10, 12:00, 11:20, 11:30, 19:00, 20:00} ",
+    "output":"3 (There are at-most three trains at a time (time between 11:00 to 11:20))"
+},
+];
